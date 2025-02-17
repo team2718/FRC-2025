37bb02013b1a6c2a234ee7fb45bb618043959bbd
Zachary <150299755+ZacharyKingery@users.noreply.github.com>
2025-02-17 16:30:01 -0600

    Imported PID to "CoralIntakeSubsystem"
    Imported PID to "CoralIntakeSubsystem"

--- src/main/java/frc/robot/subsystems/intake/CoralIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/CoralIntakeSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkBase.PersistMode;
