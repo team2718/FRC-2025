99d1881d7994b7cc7242d794302383eb5cda3dac
luxLinn <luxroboto@gmail.com>
2025-02-24 18:54:33 -0600

    Mostly completed the endEffector

--- src/main/java/frc/robot/subsystems/endeffector/EndEffectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/endeffector/EndEffectorSubsystem.java
@@ -1,38 +1,36 @@
 package frc.robot.subsystems.endeffector;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.AnalogInput;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
 
 // one or two motors, 
 public class EndEffectorSubsystem extends SubsystemBase {
     private final SparkMax endeffectormotor1;
+    private final float ampThreshold = 2;
+    private boolean noteBeThere = false;
 
-public EndEffectorSubsystem() {
-    endeffectormotor1 = new SparkMax(Constants.EndEffectorConstants.endeffectormotor1ID, MotorType.kBrushless);
-    SparkMaxConfig endeffectorConfig = new SparkMaxConfig();
-    endeffectorConfig.idleMode(IdleMode.kBrake);
-    endeffectormotor1.configure(endeffectorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-}
+    public EndEffectorSubsystem() {
+        endeffectormotor1 = new SparkMax(Constants.EndEffectorConstants.endeffectormotor1ID, MotorType.kBrushless);
+        SparkMaxConfig endeffectorConfig = new SparkMaxConfig();
+        endeffectorConfig.idleMode(IdleMode.kBrake);
+        endeffectormotor1.configure(endeffectorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
 
-public void setEndEffector (double speed) {
-    endeffectormotor1.set(speed);
-}
+    public void setEndEffector (double speed) {
+        endeffectormotor1.set(speed);
+    }
 
+    @Override
+    public void periodic() {
+        noteBeThere = (endeffectormotor1.getOutputCurrent() > ampThreshold);
+
+        SmartDashboard.putNumber("Motor Current Current ", endeffectormotor1.getOutputCurrent());
+        SmartDashboard.putBoolean("EndEffector Got Note ", noteBeThere);
+    }
 }
